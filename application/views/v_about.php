c571b6e15ea6b81b4eba2401d3d107a208af9ee2
Dmssrmdhan <Dimasramadhan0988@gmail.com>
2023-11-02 22:14:42 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,6 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+        <H1> Dimas Ramadhan </H1>
+        <H2> KELAS : 19.3A.04</H2>
+        <H3> NIM : 19220743 </H3>
+</section>
---
+++ application/views/v_index.php
@@ -0,0 +1,6 @@
+<section>
+        <h1><?php echo $judul ?></h1>
+        <H1>
+              Dimas Ramadhan 19.3A.04
+        </H1>
+</section>
